7082d2f7af2c2a67d75da795db16e01c34e3d5ff
Sarayut Kungsaranuwat <sarayut.kungsaranuway@gmail.com>
2026-01-03 16:15:51 +0700

    update AboutUs

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -5,12 +5,12 @@ function AboutUs() {
   return (
     <div className="about-us-container">
       {/* <h1 className="about-us-heading">About Us</h1> */}
-      <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity!</p>
+      <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity! We are a passionate team of plant enthusiasts dedicated to bringing the beauty of nature closer to you.</p>
       <p className="about-us-content">
         At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of 
         high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and 
         more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every 
-        plant enthusiast.
+        plant enthusiast. We are committed to providing you with the best possible experience, from selecting the perfect plant for your needs to ensuring that it thrives in your home or office.
       </p>
       {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
       <p className="about-us-content">
